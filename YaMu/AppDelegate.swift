25a5da5176c8aa1aafe90b6baba98f0970e13481
Roman <rashkur@gmail.com>
2016-11-16 02:48:07 +0200

    Initial Commit

---
+++ YaMu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  YaMu
+//
+//  Created by Roman Shkurov on 11/16/16.
+//  Copyright © 2016 Roman Shkurov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ YaMu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YaMu
+//
+//  Created by Roman Shkurov on 11/16/16.
+//  Copyright © 2016 Roman Shkurov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
